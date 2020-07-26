f4a1b2b4d7b74510f6a5b717a40e17c650956ec2
23112 <2311260561@qq.com>
2020-07-26 20:52:21 +0800

    更新邮箱

--- 发送邮件/关爱邮件/每日一句.py
+++ 发送邮件/关爱邮件/每日一句.py
@@ -72,7 +72,7 @@ class DailyGreeting(object):
         # 发件人邮箱地址
         sender = '2311260561@qq.com'
         # 客户端授权码：需要在注册邮箱后，登录进入->设置->常规设置->客户端授权码 里面进行设置
-        auth_code = 'yqfutamccqetdjfe'
+        auth_code = 'hslbjvxnfxpgdiac'
         messageObj = MIMEText(message, "html", "utf-8")
         # 设置主题
         messageObj['Subject'] = Header("每日温馨提醒~", "utf-8")
@@ -128,7 +128,7 @@ class DailyGreeting(object):
 if __name__ == '__main__':
     # 需要发送邮件的联系方式
     friend_list = [
-                   {'mail': 'q2311260561@gmail.com', 'city': '渭南'},
+                   {'mail': 'q2311260561@gmail.com', 'city': '桂林'},
                    {'mail': '3039813794@qq.com', 'city': '渭南'}, #卓越
                    {'mail': '1031708663@qq.com', 'city': '南宁'}, #桂龙
                    {'mail': '1138446882@qq.com','city':'渭南'}, #子颖
@@ -153,29 +153,29 @@ if __name__ == '__main__':
     # schedule.every().wednesday.at("13:15").do(job)  # 具体某一天某个时刻执行一次job函数
     # schedule.every(10).seconds.do(job2)    # 可以同时定时执行多个任务，但是每个任务是按顺序执行
     #
-    # SECONDS_PER_DAY = 24 * 60 * 60  # 一天时间(秒)
-    # SET_TIME = 7                    # 定时每日7点执行一次
-    # is_first = True                 # 是否第一次执行任务
-    # now = datetime.datetime.now()   # 第一次执行获取当前时间
-    # 定时执行任务逻辑
-    # while True:
-    #     cur_s = now.hour * 60 * 60 + now.minute * 60 + now.second   # 当前时间(时+分+秒)--> 秒
-    #     first_gap_s = 0     # 第一次等待的时间(秒)
-    #     if now.hour > SET_TIME:
-    #         # 当前时间小时数 大于 7点
-    #         first_gap_s = 24 * 60 * 60 - cur_s + SET_TIME * 60 * 60 # 差距 --> 秒
-    #         print("========== 大于时 ==========", first_gap_s)
-    #     elif now.hour < SET_TIME:
-    #         # 当前时间小时数 小于 7点
-    #         first_gap_s = SET_TIME * 60 * 60 - cur_s
-    #         print("========== 小于时 ==========", first_gap_s)
-    #     if is_first:
-    #         # 第一次执行需要等待的时间，即first_gap_s秒后调用main()发送邮件
-    #         time.sleep(first_gap_s)
-    #     else:
-    #         print("========== 24H 之后再次执行 ==========")
-    #         time.sleep(SECONDS_PER_DAY)     # 每24H执行一次
-    #     DailyGreeting(friend_list).main()
-    #     is_first = False    # 第一次执行完后将is_first置False
-    #     print("========== 每日华丽分割线 ==========")
+    SECONDS_PER_DAY = 24 * 60 * 60  # 一天时间(秒)
+    SET_TIME = 7                    # 定时每日7点执行一次
+    is_first = True                 # 是否第一次执行任务
+    now = datetime.datetime.now()   # 第一次执行获取当前时间
+    #定时执行任务逻辑
+    while True:
+        cur_s = now.hour * 60 * 60 + now.minute * 60 + now.second   # 当前时间(时+分+秒)--> 秒
+        first_gap_s = 0     # 第一次等待的时间(秒)
+        if now.hour > SET_TIME:
+            # 当前时间小时数 大于 7点
+            first_gap_s = 24 * 60 * 60 - cur_s + SET_TIME * 60 * 60 # 差距 --> 秒
+            print("========== 大于时 ==========", first_gap_s)
+        elif now.hour < SET_TIME:
+            # 当前时间小时数 小于 7点
+            first_gap_s = SET_TIME * 60 * 60 - cur_s
+            print("========== 小于时 ==========", first_gap_s)
+        if is_first:
+            # 第一次执行需要等待的时间，即first_gap_s秒后调用main()发送邮件
+            time.sleep(first_gap_s)
+        else:
+            print("========== 24H 之后再次执行 ==========")
+            time.sleep(SECONDS_PER_DAY)     # 每24H执行一次
+        DailyGreeting(friend_list).main()
+        is_first = False    # 第一次执行完后将is_first置False
+        print("========== 每日华丽分割线 ==========")
 
